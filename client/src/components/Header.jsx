f10a652b0dfac296a5b1d7e852c2cf5ae1beb13c
dhruvluniya11 <155226524+dhruvluniya11@users.noreply.github.com.>
2024-07-24 17:02:48 +0530

    complete sign up page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
             <div className="flex justify-between items-center max-width-6xl mx-auto p-3">
                 <Link to='/'>
                     <h1 className="font-bold text-sm:text-xl flex flex-wrap">
-                        <span className="text-slate-500">Luniya</span>
-                        <span className="text-slate-700">Estate</span>
+                        <span className="text-slate-500">Home</span>
+                        <span className="text-slate-700">Haven</span>
                     </h1>
                 </Link>
 
